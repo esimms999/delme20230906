291971063cc624dc90bffcb2861e543d5839d700
esimms999 <esimms@yahoo.com>
2023-09-06 16:14:42 +0000

    Init.

---
+++ R/add_two_numbers.R
@@ -0,0 +1,13 @@
+#' Function to add two numbers
+#'
+#' @param num1 numeric value
+#' @param num2 numeric value
+#'
+#' @return numeric value
+#' @export
+#'
+#' @examples add_two_numbers(7, 11)
+add_two_numbers <- function(num1, num2) {
+  total <- num1 + num2
+  total
+}
