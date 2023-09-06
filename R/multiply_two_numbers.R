ed03d010d3167da3584ecb34ea162b7bb175944a
esimms999 <esimms@yahoo.com>
2023-09-06 16:26:14 +0000

    Added second fn into devel.

---
+++ R/multiply_two_numbers.R
@@ -0,0 +1,13 @@
+#' Function to multiply two numbers
+#'
+#' @param num1 numeric value
+#' @param num2 numeric value
+#'
+#' @return numeric value which is the multiple of two numbers
+#' @export
+#'
+#' @examples multiply_two_numbers(7, 11)
+multiply_two_numbers <- function(num1, num2) {
+  total <- num1 * num2
+  total
+}
